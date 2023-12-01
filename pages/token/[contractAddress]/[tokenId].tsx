ce1e83ad6db606775b46b6972ad296ba2b67ba4e
Arkenia <85551951+sarp07@users.noreply.github.com>
2023-12-01 17:35:16 +0300

    Update [tokenId].tsx

--- pages/token/[contractAddress]/[tokenId].tsx
+++ pages/token/[contractAddress]/[tokenId].tsx
@@ -188,6 +188,9 @@ export default function TokenPage({ nft, contractMetadata }: Props) {
                   </div>
                 ))}
               </div>
+              <br />
+              <br />
+              <br />
             </div>
           </div>
 
